aa716bc13aa7778ffbedf0806e9ae04ca90ef408
xxibcill <36674072+xxibcill@users.noreply.github.com>
2022-07-08 00:20:49 +0700

    can insert users to remote db

--- api-server/main.go
+++ api-server/main.go
@@ -5,10 +5,23 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"net/http"
 	"os"
 	"sync"
 	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
+
+// user data
+type User struct {
+	Id            string `json:"id"`
+	Username      string `json:"username"`
+	Password      string `json:"password"`
+	Profile_image string `json:"profile_image"`
+	Joined_date   string `json:"joined_date"`
+}
 
 var (
 	db   *sql.DB
@@ -18,19 +31,45 @@ var (
 
 func main() {
 	db = getDB()
-	insert_(db)
-	getVoteData(db)
+	// insert_(db)
+
+	r := gin.Default()
+	r.Use(cors.Default())
+	r.POST("/user", insertUsers)
+
+	r.Run()
+}
+
+func insertUsers(c *gin.Context) {
+
+	var user User
+	e := c.BindJSON(&user)
+	if e != nil {
+		fmt.Println(e)
+	}
+
+	// [START cloud_sql_postgres_databasesql_connection]
+	insertVote := "INSERT INTO Users VALUES($1,$2,$3,$4,$5)"
+
+	_, err := db.Exec(insertVote, user.Id, user.Username, user.Password, user.Profile_image, user.Joined_date)
+	// [END cloud_sql_postgres_databasesql_connection]
+
+	if err != nil {
+		log.Printf("saveVote: unable to save vote: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Success"})
 }
 
 func getVoteData(db *sql.DB) {
 	fmt.Println("getVoteData")
-	t, err := currentTotals(db)
+	t := getAllUsers(db)
 	if err != nil {
 		log.Printf("renderIndex: failed to read current totals: %v", err)
 		return
 	}
 
-	log.Printf("t: %v", t.VoteMargin)
+	log.Printf("t: %v", t)
 }
 
 func insert_(db *sql.DB) {
@@ -58,12 +97,24 @@ func mustConnect() *sql.DB {
 		}
 	}
 
-	if db == nil {
-		log.Fatal("Missing database connection type. Please define one of INSTANCE_HOST, INSTANCE_UNIX_SOCKET, or INSTANCE_CONNECTION_NAME")
+	// Use a Unix socket when INSTANCE_UNIX_SOCKET (e.g., /cloudsql/proj:region:instance) is defined.
+	if os.Getenv("INSTANCE_UNIX_SOCKET") != "" {
+		db, err = connectUnixSocket()
+		if err != nil {
+			log.Fatalf("connectUnixSocket: unable to connect: %s", err)
+		}
 	}
 
-	if err := migrateDB(db); err != nil {
-		log.Fatalf("unable to create table: %s", err)
+	// Use the connector when INSTANCE_CONNECTION_NAME (proj:region:instance) is defined.
+	if os.Getenv("INSTANCE_CONNECTION_NAME") != "" {
+		db, err = connectWithConnector()
+		if err != nil {
+			log.Fatalf("connectConnector: unable to connect: %s", err)
+		}
+	}
+
+	if db == nil {
+		log.Fatal("Missing database connection type. Please define one of INSTANCE_HOST, INSTANCE_UNIX_SOCKET, or INSTANCE_CONNECTION_NAME")
 	}
 
 	return db
@@ -79,56 +130,46 @@ func getDB() *sql.DB {
 	return db
 }
 
-// migrateDB creates the votes table if it does not already exist.
-func migrateDB(db *sql.DB) error {
-	createVotes := `CREATE TABLE IF NOT EXISTS votes (
-		id SERIAL NOT NULL,
-		created_at timestamp NOT NULL,
-		candidate VARCHAR(6) NOT NULL,
-		PRIMARY KEY (id)
-	);`
-	_, err := db.Exec(createVotes)
-	return err
-}
+func getAllUsers(db *sql.DB) []User {
+	queryString := "SELECT * FROM USERS"
+	row, err := db.Query(queryString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
 
-// votingData is used to pass data to the HTML template.
-type votingData struct {
-	TabsCount   int
-	SpacesCount int
-	VoteMargin  string
-	RecentVotes []vote
+	result := []User{}
+
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var id string
+		var username string
+		var password string
+		var profile_image string
+		var joined_date string
+		row.Scan(&id, &username, &password, &profile_image, &joined_date)
+		ingredient := User{id, username, password, profile_image, joined_date}
+		result = append(result, ingredient)
+	}
+
+	return result
 }
 
 // currentTotals retrieves all voting data from the database.
-func currentTotals(db *sql.DB) (votingData, error) {
+func currentTotals(db *sql.DB) (int, error) {
 	fmt.Println("currentTotals")
+
 	var (
-		tabs   int
-		spaces int
+		count int
 	)
-	err := db.QueryRow("SELECT count(id) FROM votes WHERE candidate='TABS'").Scan(&tabs)
+
+	err := db.QueryRow("SELECT * FROM USERS").Scan(&count)
 	if err != nil {
-		return votingData{}, fmt.Errorf("DB.QueryRow: %v", err)
-	}
-	err = db.QueryRow("SELECT count(id) FROM votes WHERE candidate='SPACES'").Scan(&spaces)
-	if err != nil {
-		return votingData{}, fmt.Errorf("DB.QueryRow: %v", err)
+		return 0, fmt.Errorf("DB.QueryRow: %v", err)
 	}
 
-	recent, err := recentVotes(db)
-	if err != nil {
-		return votingData{}, fmt.Errorf("recentVotes: %v", err)
-	}
+	log.Printf("tabs: %v", count)
 
-	log.Printf("tabs: %v", tabs)
-	log.Printf("spaces: %v", spaces)
-
-	return votingData{
-		TabsCount:   tabs,
-		SpacesCount: spaces,
-		VoteMargin:  formatMargin(tabs, spaces),
-		RecentVotes: recent,
-	}, nil
+	return count, nil
 }
 
 // formatMargin calculates the difference between votes and returns a human
